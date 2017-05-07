ba4cdc027122b58d0a4b5d51c4636e27e67a818f
skumailraza <14beseskumailraza@seecs.edu.pk>
2017-05-07 12:48:12 +0500

    Upgraded to Tesseract 4

---
+++ tess-two/jni/com_googlecode_tesseract_android/src/cmake-build-debug/CMakeFiles/tesseractmain.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/srq/Installations/tesseract/api/tesseractmain.cpp" "/home/srq/Installations/tesseract/cmake-build-debug/CMakeFiles/tesseractmain.dir/api/tesseractmain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_CONFIG_H"
+  "TESS_IMPORTS"
+  "USE_STD_NAMESPACE=1"
+  "WINDLLNAME=\"libtesseract305.dll\""
+  "_SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/leptonica"
+  "."
+  "../api"
+  "../arch"
+  "../ccmain"
+  "../ccstruct"
+  "../ccutil"
+  "../classify"
+  "../cutil"
+  "../dict"
+  "../lstm"
+  "../opencl"
+  "../textord"
+  "../vs2010/port"
+  "../viewer"
+  "../wordrec"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/srq/Installations/tesseract/cmake-build-debug/CMakeFiles/tesseract.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
